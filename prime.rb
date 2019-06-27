a886eebc2bf323cc9bc8c8ccca529e7d8c4d4cc1
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 05:28:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if num > 1
+    natural_number = (2..num).to_a
+    natural_number.count{|natural| num%natural==0}==1
+  else
+    return false
+  end
+end
+
+  
