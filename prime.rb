fda2815e0b69ced1f07d00fd2db4baabcb9a566a
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 05:27:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   if num > 1
     natural_number = (2..num).to_a
-    natural_number.count{|natural| num%natural==0}!=1
+    natural_number.count{|natural| num%natural==0}==1
   else
     return false
   end
